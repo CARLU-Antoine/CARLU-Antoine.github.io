053408887f961fcbc4bb1a81582aa9f98f31cb93
Antoine Carlu <antoinecarlu@macbookpro.home>
2025-07-16 21:31:49 +0200

    Modif style frise

--- js/cards-formations.js
+++ js/cards-formations.js
@@ -1,5 +1,5 @@
-const stack = document.querySelector(".container-cards");
-const cards = Array.from(stack.children).reverse().filter((child) => child.classList.contains("card"));
+const containerCardsFromations = document.querySelector(".container-cards-formations");
+const cards = Array.from(containerCardsFromations.children).reverse().filter((child) => child.classList.contains("card"));
 
 let currentImageIndex = -1; // Index de l'image actuelle
 let images = []; // Tableau pour stocker les sources des images
@@ -11,17 +11,17 @@ cards.forEach((card) => {
 });
 
 // Réorganiser les cartes
-cards.forEach((card) => stack.appendChild(card));
+cards.forEach((card) => containerCardsFromations.appendChild(card));
 
 // Fonction pour déplacer la carte
 function moveCard() {
-    const lastCard = stack.lastElementChild;
+    const lastCard = containerCardsFromations.lastElementChild;
     if (lastCard.classList.contains("card")) {
         lastCard.classList.add("swap");
 
         setTimeout(() => {
             lastCard.classList.remove("swap");
-            stack.insertBefore(lastCard, stack.firstElementChild);
+            containerCardsFromations.insertBefore(lastCard, containerCardsFromations.firstElementChild);
         }, 1200);
     }
 }
