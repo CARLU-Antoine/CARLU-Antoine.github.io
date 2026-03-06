bbe018afbd81a95f3ad4770d66ba92f642b844e1
Antoine Carlu <antoinecarlu@macbookpro.home>
2026-03-06 21:40:52 +0100

    Création navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NavbarComponent },
+];
---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css']
+})
+export class NavbarComponent {
+  constructor(private router: Router) {}
+
+  navigate(route: string) {
+    this.router.navigate([route]);
+  }
+}
